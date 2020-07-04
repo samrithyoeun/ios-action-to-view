271210500acc4a87f93c5c02655812884d478d8c
sam yoeun <sam.yoeun@pathmazing.com>
2020-07-04 14:32:39 +0700

    Initial Commit

---
+++ ios-action-to-view/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-action-to-view
+//
+//  Created by Samrith Yoeun on 7/4/20.
+//  Copyright © 2020 Sammi Yoeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
